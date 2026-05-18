2cc823b0322a567955a25021c8dde14462dcac7a
rendylong <didobang@foxmail.com>
2026-05-18 14:28:38 +0800

    feat: cobra scaffold with root command and persistent flags

---
+++ cmd/root.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	env    string
+	format string
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "crm-cli",
+	Short: "Orvibo CRM command-line tool",
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVar(&env, "env", "prod", "Environment: prod, test, local")
+	rootCmd.PersistentFlags().StringVar(&format, "format", "json", "Output format: json, table")
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
